01e0dbbbcab9517a59de51e893b5b052ed569844
Parth Desai <Parth Desai>
2016-01-03 20:19:00 -0500

    Server and Client

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,34 @@
-
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 public class Client {
-
+	private final  int PORT_NUM= 3030;
+	public Client() {
+	}
+	public boolean send_message(String message, String host_name) {
+		try (
+			//Establish connection
+			Socket socket = new Socket(host_name, PORT_NUM);
+			//Output to socket
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+		) {		
+			out.println(message);
+		} catch(UnknownHostException e) {
+			System.err.println("Unknown host");
+			return false;
+			
+		} catch(IOException e) {
+			 System.err.println("Couldn't get I/O for the connection to " +
+					 host_name);
+			 return false;
+		} 
+		return true;
+	}
+	
+	
+	
+	
+	
+	
 }
--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,42 @@
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket; 
 public class Server {
-
+	private final  int PORT_NUM= 3030;
+	public Server() {
+	}
+	public void listen_messages() {
+		try {
+			ServerSocket serverSocket = new ServerSocket(PORT_NUM);
+	
+			while(true) {
+				try (
+					Socket clientSocket = serverSocket.accept();  
+					PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);                   
+			        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		        ) {
+					String inputLine;
+		            while ((inputLine = in.readLine()) != null) {
+		                out.println(inputLine);
+		            }
+				}
+				catch(IOException e) {
+					System.out.println("Exception");
+					System.out.println(e.getMessage());
+					serverSocket.close();
+					break;
+				}
+			}
+			serverSocket.close();
+		}
+		catch(Exception e) {
+			 System.out.println("Exception caught when trying to listen on port " + PORT_NUM + " or listening for a connection");
+		     System.out.println(e.getMessage());
+			 return;
+		}
+	}
+	
 }
