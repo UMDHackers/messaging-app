df30b06f800bce68e4710543edcfe2ad8f430d02
Parth Desai <parthdesai95@gmail.com>
2016-01-04 10:29:24 -0500

    Changes

--- src/KongoMessenger.java
+++ src/KongoMessenger.java
@@ -1,4 +1,9 @@
 
 public class KongoMessenger {
-	
+	public static void main(String args[]) {
+		Client client = new Client();
+		Server server = new Server();
+		server.listen_messages();
+		
+	}
 }
